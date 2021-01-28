4da29ee41f913802e2602a1197e264bd9a558cac
Sagramielgo <sagramielgo@gmail.com>
2021-01-28 11:37:31 +0100

    clean code

--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
 'use strict';
 
-/* 1 Escuchar botón de prueba
-      1.1 cada click suma 1 = bucle
-
-2 Generar número aleatorio entre 0 y 100
-
-3 Leer número en input
-    3.1 es mayor que el aleatorio = responder el núm es demasiado alto
-    3.2 es menor que el aleatorio = responder es demasiado bajo
-    3.3 es igual que el aleatorio = enhorabuena
-    3.4  menor o mayor que 100 no válido
-
-4 escuchar botón reset= volver a 0
-*/
-
-//VARIABLES
 const tryButtonElement = document.querySelector('.tryButton-js');
 const inputNumberElement = document.querySelector('.numberInput-js');
 const tryNumberElement = document.querySelector('.tryNumber-js');
@@ -28,14 +13,12 @@ const randomNumber = getRandomNumber(max);
 
 console.log(`El número aleatorio es: ${randomNumber}`);
 
-//COUNTER
 let acc = 0;
 const timesTry = () => {
   acc += 1;
   counterTimesElement.innerHTML = `${acc}`;
 };
 
-//EVENT Times FUNCTION
 function priceAnswer() {
   const numberIntroduced = inputNumberElement.value;
   const userIntroduceValue = userIntroduceValue;
@@ -53,12 +36,10 @@ function priceAnswer() {
     tryNumberElement.innerHTML = 'El número es demasiado bajo';
   }
 }
-priceAnswer();
+
 function start(event) {
   event.preventDefault();
-
+  priceAnswer();
   timesTry();
 }
-
-//LISTENERS
 tryButtonElement.addEventListener('click', start);
